ce74fe89ba3727ea5ed6267a60d4951a65d42cd5
Gustavo Percoski <gustavopercoski2@gmail.com>
2026-02-16 19:33:01 -0300

    feat: implementando estrutura do swiper

--- src/components/ui/HeroSlider.tsx
+++ src/components/ui/HeroSlider.tsx
@@ -1,3 +1,7 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+
 import ps5Pro from "../../assets/ps5-pro.webp";
 import macbookPro from "../../assets/MacBook-Pro.png";
 import s25Ultra from "../../assets/s25-ultra.png";
@@ -5,23 +9,18 @@ import s25Ultra from "../../assets/s25-ultra.png";
 const slides = [
   {
     id: 1,
-    brand: "Sony",
     title: "PlayStation 5 Pro",
     desc: "Console de nova geração",
     img: ps5Pro,
   },
-
   {
     id: 2,
-    brand: "Apple",
     title: "MacBook Pro M3",
     desc: "Notebook profissional",
     img: macbookPro,
   },
-
   {
     id: 3,
-    brand: "Samsung",
     title: "Galaxy S25 Ultra",
     desc: "Smartphone premium",
     img: s25Ultra,
@@ -30,16 +29,14 @@ const slides = [
 
 export function HeroSlider() {
   return (
-    <div className="w-full bg-slate-900">
-      <div className="container mx-auto py-20 text-white">
-        {slides.map((slide) => (
-          <div key={slide.id}>
-            <h2>{slide.title}</h2>
+    <Swiper>
+      {slides.map((slide) => (
+        <SwiperSlide key={slide.id}>
+          <h2>{slide.title}</h2>
 
-            <p>{slide.desc}</p>
-          </div>
-        ))}
-      </div>
-    </div>
+          <p>{slide.desc}</p>
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
 }
