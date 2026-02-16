8a9cede915a1faad7ebb471331b3a68d811af57b
Gustavo Percoski <gustavopercoski2@gmail.com>
2026-02-16 19:40:57 -0300

    feat: add imagens aos slides

--- src/components/ui/HeroSlider.tsx
+++ src/components/ui/HeroSlider.tsx
@@ -1,42 +1,55 @@
-import { Swiper, SwiperSlide } from "swiper/react";
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-import "swiper/css";
+import 'swiper/css';
 
-import ps5Pro from "../../assets/ps5-pro.webp";
-import macbookPro from "../../assets/MacBook-Pro.png";
-import s25Ultra from "../../assets/s25-ultra.png";
+import ps5Pro from '../../assets/ps5-pro.webp';
+import macbookPro from '../../assets/MacBook-Pro.png';
+import s25Ultra from '../../assets/s25-ultra.png';
 
 const slides = [
   {
     id: 1,
-    title: "PlayStation 5 Pro",
-    desc: "Console de nova geração",
+    title: 'PlayStation 5 Pro',
     img: ps5Pro,
   },
   {
     id: 2,
-    title: "MacBook Pro M3",
-    desc: "Notebook profissional",
+    title: 'MacBook Pro M3',
     img: macbookPro,
   },
   {
     id: 3,
-    title: "Galaxy S25 Ultra",
-    desc: "Smartphone premium",
+    title: 'Galaxy S25 Ultra',
     img: s25Ultra,
   },
 ];
 
 export function HeroSlider() {
-  return (
-    <Swiper>
-      {slides.map((slide) => (
-        <SwiperSlide key={slide.id}>
-          <h2>{slide.title}</h2>
 
-          <p>{slide.desc}</p>
+  return (
+
+    <Swiper>
+
+      {slides.map((slide) => (
+
+        <SwiperSlide key={slide.id}>
+
+          <img
+            src={slide.img}
+            alt={slide.title}
+            className="h-[300px]"
+          />
+
+          <h2>
+            {slide.title}
+          </h2>
+
         </SwiperSlide>
+
       ))}
+
     </Swiper>
+
   );
+
 }
