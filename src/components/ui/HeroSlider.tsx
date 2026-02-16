9051391019c3505b723fdb5f7bd098678e34a308
Gustavo Percoski <gustavopercoski2@gmail.com>
2026-02-16 18:50:32 -0300

    feat: criando componente HeroSlider e add na Home

---
+++ src/components/ui/HeroSlider.tsx
@@ -0,0 +1,13 @@
+export function HeroSlider() {
+  return (
+    <div className="w-full bg-slate-900">
+      <div className="container mx-auto py-20 text-white">
+        <h1 className="text-4xl font-bold">Hero Slider</h1>
+
+        <p className="mt-4 text-white/70">
+          área onde vai ficar o slider principal
+        </p>
+      </div>
+    </div>
+  );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,9 @@
+import { HeroSlider } from "../components/ui/HeroSlider";
+
 export function Home() {
   return (
-    <div className="py-10">
-      <h2 className="text-3xl font-bold mb-4">Bem-vindo</h2>
-      <p className="text-gray-600">Espaço reservado para listar os produtos</p>
-    </div>
+    <>
+      <HeroSlider />
+    </>
   );
 }
